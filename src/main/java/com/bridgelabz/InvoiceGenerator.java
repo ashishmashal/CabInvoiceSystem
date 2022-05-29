6e70ae77a741fdeb318ef042f52a87d06b525fab
ashishmashal <ashishmashal5@gmail.com>
2022-05-29 13:45:24 +0530

    Added:Given Distance And Time Should Return Total Fare

---
+++ src/main/java/com/bridgelabz/InvoiceGenerator.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+public class InvoiceGenerator {
+//	public static void main(String[] args) {
+//		System.out.println("Welcome To Cab Invoice System");
+//	}
+private static  int costPerMinute = 1;
+	private static  double minimumCostPerKilometer = 10;
+	private static  double minimumFare = 5;
+	public double CalculateFare(double distance, int time)
+	{
+		double totalFare = distance * minimumCostPerKilometer + time * costPerMinute;
+		if (totalFare < minimumFare)
+		{
+			return minimumFare;
+		}
+		return totalFare;
+	}
+}
--- src/main/java/com/bridgelabz/InvoiceGerator.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz;
-
-public class InvoiceGerator {
-	public static void main(String[] args) {
-		System.out.println("Welcome To Cab Invoice System");
-	}
-}
